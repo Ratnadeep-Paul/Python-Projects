3ea90fc293f78620d59385b082a0efce345ae4de
saad-sifar <66911402+saad-sifar@users.noreply.github.com>
2020-10-29 13:02:19 +0530

    Your original code was a bit redundant, so I made it much shorter
    I haven't tested if this works yet, i'm also not sure where you got the exit() function from.
    This is my first time contributing to someone's project too, so I hope my changes were helpful.
    If you can, please test the changes before merging my pull request

--- Guess The Number/Guess The Number Game.py
+++ Guess The Number/Guess The Number Game.py
@@ -1,136 +1,40 @@
 import random
 def guess_number():
     print("Welcome To The Number Guessing Game")
-    print("You Have 10 Chance To Guess The Number.. (The Number is between 1-50)")
+    print("You Have 10 Chances To Guess The Number.. (The Number is between 1-50)")
     print("Enter Your First Guess For The Number")
+    guesses = 0
     gnum = random.randint(1, 50)
     finpu = int(input(":-"))
 
-    if(finpu>gnum):
-        print("Wrong!!! You Select A Greater Number")
-        print("9 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
+    while finpu != gnum and guesses < 10:           
+        if finpu > gnum:
+            guesses += 1
+            print("Wrong!!! You Selected A Bigger Number")
+            print(f"{10 - guesses} Chances Left")
+            print("\nEnter A New Number")
+            finpu = int(input(":-"))
+        
+        elif finpu < gnum:
+            guesses += 1
+            print("Wrong!!! You Selected A Smaller Number")
+            print(f"{10 - guesses} Chances Left")
+            print("\nEnter A New Number")
+            finpu = int(input(":-"))
 
-    elif(finpu<gnum):
-        print("Wrong!!! You Select A Lesser Number")
-        print("9 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
-
-
-    if (finpu > gnum):
-        print("Wrong!!! You Select A Greater Number")
-        print("8 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
-
-    elif (finpu < gnum):
-        print("Wrong!!! You Select A Lesser Number")
-        print("8 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
-
-
-    if (finpu > gnum):
-        print("Wrong!!! You Select A Greater Number")
-        print("7 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
-
-    elif (finpu < gnum):
-        print("Wrong!!! You Select A Lesser Number")
-        print("7 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
-
-    if (finpu > gnum):
-        print("Wrong!!! You Select A Greater Number")
-        print("6 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
-
-    elif (finpu < gnum):
-        print("Wrong!!! You Select A Lesser Number")
-        print("6 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
-
-    if (finpu > gnum):
-        print("Wrong!!! You Select A Greater Number")
-        print("5 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
-
-    elif (finpu < gnum):
-        print("Wrong!!! You Select A Lesser Number")
-        print("5 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
-
-    if (finpu > gnum):
-        print("Wrong!!! You Select A Greater Number")
-        print("4 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
-
-    elif (finpu < gnum):
-        print("Wrong!!! You Select A Lesser Number")
-        print("4 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
-
-    if (finpu > gnum):
-        print("Wrong!!! You Select A Greater Number")
-        print("3 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
-
-    elif (finpu < gnum):
-        print("Wrong!!! You Select A Lesser Number")
-        print("3 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
-
-
-    if (finpu > gnum):
-        print("Wrong!!! You Select A Greater Number")
-        print("2 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
-
-    elif (finpu < gnum):
-        print("Wrong!!! You Select A Lesser Number")
-        print("2 Chances Left")
-        print("\nEnter A New Number")
-        finpu = int(input(":-"))
-
-    if (finpu > gnum):
-        print("Wrong!!! You Select A Greater Number")
-        print("It's Your Last Chance")
-        print("\nEnter A Last Number")
-        finpu = int(input(":-"))
-
-    elif (finpu < gnum):
-        print("Wrong!!! You Select A Lesser Number")
-        print("It's Your Last Chance")
-        print("\nEnter A Last Number")
-        finpu = int(input(":-"))
-
-    if (finpu!=gnum):
-        print("\nGame Over")
-        print("You Can't Guess The Number")
-
-    else:
-        print("\nCongratulation!! You Win The Game")
-
-    print("Thanks For Playing This Game")
-    print("\nDo you Want to play it again??\n     Play--1 , Exit--2")
-    more = int(input(":-"))
-    if more==1:
-        print(guess_number())
-    else:
-        print("Exiting....")
-        exit()
-
+    if guesses > 10:
+        print("Sorry, you had too many guesses")
+        print(f'The number was {gnum}')
+        
+    elif finpu == gnum:
+        print("\nCongratulations!! You Win The Game")
+        print("Thanks For Playing This Game")
+        print("\nDo you Want to play it again??\n     Play--1 , Exit--2")
+        more = int(input(":-"))
+        if more==1:
+            print(guess_number())
+        else:
+            print("Exiting....")
+            exit()
+                
 print(guess_number())
